ba205b0ef5045da9d893a99250a9349f190513fc
+ <+>
2025-01-22 14:30:57 +0300

    updated products.jsx

--- src/data/Products.jsx
+++ src/data/Products.jsx
@@ -1,13 +1,13 @@
 const products=[
-    { "id": 1, "name": "T-Shirt", "price": 20 ,"image":"./T-shirt.png"},
+    { "id": 1, "name": "T-Shirt", "price": 20 ,"image":"/T-shirt.png"},
 
-    { "id": 2, "name": "Jeans", "price": 40 ,"image":"./Jeans.png"},
+    { "id": 2, "name": "Jeans", "price": 40 ,"image":"/Jeans.png"},
   
-    { "id": 3, "name": "Sneakers", "price": 60,"image":"./sneakers.png" },
+    { "id": 3, "name": "Sneakers", "price": 60,"image":"/sneakers.png" },
   
-    { "id": 4, "name": "Hat", "price": 15, "image":"./Hat.png"},
+    { "id": 4, "name": "Hat", "price": 15, "image":"/Hat.png"},
   
-    { "id": 5, "name": "Socks", "price": 5, "image":"./Socks.png"}
+    { "id": 5, "name": "Socks", "price": 5, "image":"/Socks.png"}
   
   
 ]
